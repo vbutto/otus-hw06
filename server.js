2297b6c37eca1faa06239a209676df96024be0cc
Vyacheslav Butyrkin <slava.butyrkin@gmail.com>
2025-09-11 00:11:55 +0200

    New url for health-check

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const server = http.createServer((req, res) => {
   const now = new Date().toISOString();
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello from HW06! Time: ${now}\n`);
+
+  // Добавляем простейшую проверку здоровья
+  if (req.url === '/health') {
+    res.end('OK');
+  } else {
+    res.end(`Hello from HW06! Time: ${now}\n`);
+  }
 });
 
 server.listen(port, () => {
